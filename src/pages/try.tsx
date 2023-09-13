0edf30035c76e2fd98c8918b193277fb42cce1ac
ponza555 <ponpon13173@gmail.com>
2023-09-13 08:50:08 +0700

    add link

---
+++ src/pages/try.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Testlink from "../components/events/Testlinkwithmap"
+
+
+const Try = () =>{
+    return(
+        <div className=" h-screen  bg-gradient-to-b from-green-300 to-green-600 ">
+            <Testlink></Testlink>
+            
+        </div> 
+        
+        
+    )
+
+}
+export default Try
